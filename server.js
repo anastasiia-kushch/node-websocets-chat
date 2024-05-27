5b50f4984794d1a621e40b645d41e28a42ea9ada
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-05-27 09:47:19 +0200

    editing

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+import express from 'express';
+import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 
-const server = new WebSocketServer({ port: 5500 });
+const app = express();
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
